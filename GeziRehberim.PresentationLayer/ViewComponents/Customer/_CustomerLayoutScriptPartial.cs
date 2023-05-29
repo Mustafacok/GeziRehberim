650b3a7416008832c4efcee23376633d78f10ad1
Mustafa ÇOK <96659190+Mustafacok@users.noreply.github.com>
2023-05-29 21:39:41 +0300

    ViewComponents oluşturuldu.

---
+++ GeziRehberim.PresentationLayer/ViewComponents/Customer/_CustomerLayoutScriptPartial.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GeziRehberim.PresentationLayer.ViewComponents.Customer
+{
+    public class _CustomerLayoutScriptPartial: ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
